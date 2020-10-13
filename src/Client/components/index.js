ae9654dc3ad1911ea3c3959bece765c274c7eaff
dara0799 <daraayufahira.daf@gmail.com>
2020-10-13 09:23:13 +0700

    menambahkan file Navbar dan ListItems

--- src/Client/components/index.js
+++ src/Client/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./Header";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 import TextFieldComp from "./TextFieldComp";
-export { Header, Footer, TextFieldComp };
+export { Navbar, Footer, TextFieldComp };
