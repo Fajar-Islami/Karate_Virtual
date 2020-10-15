6ccfae351700dd4a267eef97fea152a08176dd76
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-10-15 23:26:48 +0700

    Rincian pembayaran

--- src/Client/pages/Profile/Pembayaran/PembayaranAktif/RincianPembayaran.js
+++ src/Client/pages/Profile/Pembayaran/PembayaranAktif/RincianPembayaran.js
@@ -1,56 +1,202 @@
-import React from "react";
+import React, { Fragment } from "react";
+//style
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+//component
+import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import { green } from "@material-ui/core/colors";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import { withStyles } from "@material-ui/core";
 
-const GreenCheckbox = withStyles({
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontWeight: "600",
+    fontSize: "24px",
+  },
+  pengumumanBayar: {
+    textAlign: "center",
+    color: "#F90505",
+  },
+  rincian: {
+    fontFamily: "Poppins !important",
+    marginTop: 30,
+    marginLeft: 30,
+    width: "100%",
+    maxWidth: 500,
+    height: "100%",
+    maxHeight: 300,
+    fontSize: 16,
+  },
+  boxTable: {
+    marginTop: 20,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  table: {
+    minWidth: 500,
+  },
+  paperInfo: {
+    marginTop: 30,
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 50,
+    backgroundColor: "#757575",
+    color: "#ffffff",
+    justifyContent: "center",
+  },
+  gridContainer: {
+    marginLeft: 30,
+    marginRight: 30,
+  },
+}));
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: "#039be5",
+    color: theme.palette.common.white,
+    fontSize: 16,
+  },
+  body: {
+    fontSize: 14,
+  },
+  footer: {
+    backgroundColor: "#039be5",
+    color: theme.palette.common.white,
+    fontSize: 16,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
   root: {
-    "&$checked": {
-      color: green[600],
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
     },
   },
-  checked: {},
-})((props) => <Checkbox color="default" {...props} />);
+}))(TableRow);
 
-export default function RincianPembayaran() {
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Shipping address
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField required id="firstName" name="firstName" label="First name" fullWidth autoComplete="given-name" />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField required id="lastName" name="lastName" label="Last name" fullWidth autoComplete="family-name" />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField required id="address1" name="address1" label="Address line 1" fullWidth autoComplete="shipping address-line1" />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField id="address2" name="address2" label="Address line 2" fullWidth autoComplete="shipping address-line2" />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField required id="city" name="city" label="City" fullWidth autoComplete="shipping address-level2" />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="state" name="state" label="State/Province/Region" fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField required id="zip" name="zip" label="Zip / Postal code" fullWidth autoComplete="shipping postal-code" />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField required id="country" name="country" label="Country" fullWidth autoComplete="shipping country" />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel control={<GreenCheckbox name="checkedG" value="yes" />} label="Use this address for payment details" />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+function createData(nomor, rincianBayar, hargaSatuan, kuantitas, subTotal) {
+  return { nomor, rincianBayar, hargaSatuan, kuantitas, subTotal };
 }
+function createDataKet(nomor, klasifikasi, perorangan, beregu) {
+  return { nomor, klasifikasi, perorangan, beregu };
+}
+
+const rows = [createData(1, "Peserta Open Perorangan", 175000, 2, 350000), createData(2, "Peserta Open Beregu", 300000, 1, 300000), createData(3, "Peserta Festival", 195000, 1, 195000)];
+const rows1 = [createDataKet(1, "Pra-pemula", 0, 1), createDataKet(2, "Kadet", 1, 1), createDataKet(3, "Yunior", 1, 1), createDataKet(4, "Senior", 1, 1)];
+const RincianPembayaran = () => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Box className={classes.rincian}>
+        <span className={classes.title}>Rincian Pembayaran</span>
+        <div>
+          Nama Dojo: Phoenix
+          <br />
+          Nama Pengguna: Mawang
+          <br />
+          Nomor Telepon: 082112345678
+        </div>
+      </Box>
+      <Box display="flex" justifyContent="center" className={classes.boxTable}>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>No.</StyledTableCell>
+                <StyledTableCell align="left">Rincian Pembayaran</StyledTableCell>
+                <StyledTableCell align="left">Harga Satuan</StyledTableCell>
+                <StyledTableCell align="left">Kuantitas</StyledTableCell>
+                <StyledTableCell align="left">Subtotal</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.nomor}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.nomor}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{row.rincianBayar}</StyledTableCell>
+                  <StyledTableCell align="left">{row.hargaSatuan}</StyledTableCell>
+                  <StyledTableCell align="left">{row.kuantitas}</StyledTableCell>
+                  <StyledTableCell align="left">{row.subTotal}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <StyledTableRow>
+                <StyledTableCell></StyledTableCell>
+                <StyledTableCell align="left"></StyledTableCell>
+                <StyledTableCell align="left"></StyledTableCell>
+                <StyledTableCell align="left">Total Pembayaran</StyledTableCell>
+                <StyledTableCell align="left">845000</StyledTableCell>
+              </StyledTableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </Box>
+
+      <Box className={classes.rincian}>
+        <span className={classes.title}>Keterangan Klasifikasi</span>
+      </Box>
+      <Box display="flex" justifyContent="center" className={classes.boxTable}>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>No.</StyledTableCell>
+                <StyledTableCell align="left">Klasifikasi</StyledTableCell>
+                <StyledTableCell align="left">Peorangan</StyledTableCell>
+                <StyledTableCell align="left">Beregu</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows1.map((row) => (
+                <StyledTableRow key={row.nomor}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.nomor}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{row.klasifikasi}</StyledTableCell>
+                  <StyledTableCell align="left">{row.perorangan}</StyledTableCell>
+                  <StyledTableCell align="left">{row.beregu}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <StyledTableRow>
+                <StyledTableCell></StyledTableCell>
+                <StyledTableCell align="left"></StyledTableCell>
+                <StyledTableCell align="left">Total Peserta</StyledTableCell>
+                <StyledTableCell align="left">3</StyledTableCell>
+              </StyledTableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </Box>
+
+      <Paper elevation={3} className={classes.paperInfo}>
+        <div className={classes.gridContainer}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} xs={3}>
+              <h2 style={{ fontWeight: "bolder", textAlign: "center" }}>Metode Pembayaran</h2>
+            </Grid>
+            <Grid item xs={12} xs={9}>
+              <p style={{ textAlign: "justify" }}>
+                Biaya pedaftaran ditransfer ke rekening panitia berikut: <strong>Bank DKI No: 52723090450 a.n. Eko Supriyanto.</strong>
+                <br />
+                Peserta mentransfer sesuai jadwal yang ditentukan dan struk bukti transfer dikirim melalui Whatsapp 0859-2119-4336.
+              </p>
+            </Grid>
+          </Grid>
+        </div>
+      </Paper>
+    </Fragment>
+  );
+};
+
+export default RincianPembayaran;
