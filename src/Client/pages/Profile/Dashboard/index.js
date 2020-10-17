c4d19536b47f0cc6e30d9ff88411a07e34c50f20
jrarkaan <rakajanitraa@hotmail.com>
2020-10-17 08:06:55 +0700

    menambah file dahsboard, css, dan mengkodingnya

--- src/Client/pages/Profile/Dashboard/index.js
+++ src/Client/pages/Profile/Dashboard/index.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import PersonIcon  from "@material-ui/icons/Person";
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import FlagIcon from '@material-ui/icons/Flag';
+
+import cx from "classnames";
+// importing styles
+import styles from "./index.module.css";
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.single)}>
+          <Typography color="textSecondary" gutterBottom>Peserta Open Single</Typography>
+          <div className={styles.app__parent}>
+            <div className={styles.app__left}>
+              <PersonIcon style={{ fontSize: 50, color: "ced4da"}}/>
+            </div>
+            <div className={styles.app__right}>
+              <Typography variant="h4"> 7</Typography>
+            </div>
+          </div>
+        </Grid>
+
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.double)}>
+          <Typography color="textSecondary" gutterBottom>Peserta Open Group</Typography>
+          <div className={styles.app__parent}>
+            <div className={styles.app__left}>
+              <PeopleAltIcon style={{ fontSize: 50, color: "ced4da"}}/>
+            </div>
+            <div className={styles.app__right}>
+              <Typography variant="h4">3</Typography>
+            </div>
+          </div>
+        </Grid>
+        
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.festival)}>
+          <Typography color="textSecondary" gutterBottom>Peserta Open Festival</Typography>
+          <div className={styles.app__parent}>
+            <div className={styles.app__left}>
+              <FlagIcon style={{ fontSize: 50, color: "ced4da"}}/>
+            </div>
+            <div className={styles.app__right}>
+              <Typography variant="h4">2</Typography>
+            </div>
+          </div>
+        </Grid>
+        
+      </Grid>
+
+     
     </div>
   );
 }
