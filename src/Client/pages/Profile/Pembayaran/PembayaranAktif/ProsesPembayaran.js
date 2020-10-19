e15862392bce33edad8dd51f6e5577c4aa54ba7d
jrarkaan <rakajanitraa@hotmail.com>
2020-10-19 12:25:54 +0700

    memperbaiki sintaksis di proses pembayaran.js

--- src/Client/pages/Profile/Pembayaran/PembayaranAktif/ProsesPembayaran.js
+++ src/Client/pages/Profile/Pembayaran/PembayaranAktif/ProsesPembayaran.js
@@ -1,19 +1,13 @@
 import React, { Fragment } from "react";
 //style
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-//component
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableFooter from "@material-ui/core/TableFooter";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+// importing Material-UI Components
+import { Paper, Box } from "@material-ui/core";
 import { ScrollToTop } from "../../../../config";
+// importing components
+import { TablePembayaran } from "../../../../components";
+// importing Data
+import { rincianPembayaran, klasifikasiPembayaran } from "./data.jsx";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -56,36 +50,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: "#039be5",
-    color: theme.palette.common.white,
-    fontSize: 16,
-  },
-  body: {
-    fontSize: 14,
-  },
-  footer: {
-    backgroundColor: "#039be5",
-    color: theme.palette.common.white,
-    fontSize: 16,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-function createData(nomor, rincianBayar, hargaSatuan, kuantitas, subTotal) {
-  return { nomor, rincianBayar, hargaSatuan, kuantitas, subTotal };
-}
-
-const rows = [createData(1, "Peserta Open Perorangan", 175000, 2, 350000), createData(2, "Peserta Open Beregu", 300000, 1, 300000), createData(3, "Peserta Festival", 195000, 1, 195000)];
-
 const ProsesPembayaran = () => {
   const classes = useStyles();
   return (
@@ -118,50 +82,9 @@ const ProsesPembayaran = () => {
           Nomor Transaksi: TR-10-13-2020-001
         </div>
       </Box>
-      <Box display="flex" justifyContent="center" className={classes.boxTable}>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell align="center" width="8%">
-                  No.
-                </StyledTableCell>
-                <StyledTableCell align="left">Rincian Pembayaran</StyledTableCell>
-                <StyledTableCell align="center" width="15%">
-                  Harga Satuan
-                </StyledTableCell>
-                <StyledTableCell align="center" width="15%">
-                  Kuantitas
-                </StyledTableCell>
-                <StyledTableCell align="center" width="15%">
-                  Subtotal
-                </StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <StyledTableRow hover key={row.nomor}>
-                  <StyledTableCell align="center">{row.nomor}</StyledTableCell>
-                  <StyledTableCell align="left">{row.rincianBayar}</StyledTableCell>
-                  <StyledTableCell align="center">{row.hargaSatuan}</StyledTableCell>
-                  <StyledTableCell align="center">{row.kuantitas}</StyledTableCell>
-                  <StyledTableCell align="center">{row.subTotal}</StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <StyledTableRow>
-                <StyledTableCell component="th" scope="row" align="center" colspan={4}>
-                  Total Pembayaran
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row" align="center">
-                  845000
-                </StyledTableCell>
-              </StyledTableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
-      </Box>
+
+      <TablePembayaran rincianPembayaran={rincianPembayaran} />
+      
       <Paper elevation={3} className={classes.paperInfo}>
         <div className={classes.gridContainer}>
           <h2 style={{ fontWeight: "bolder", textAlign: "center" }}>Metode Pembayaran</h2>
