fde3ef41a12e29a7c44f991f539cfdb8951cd3ad
jrarkaan <rakajanitraa@hotmail.com>
2020-10-19 13:21:48 +0700

    menambahkan file backup dan mengkoding beberapa file

--- src/Client/components/Table/TableKlasifikasi/TableKlasifikasi.jsx
+++ src/Client/components/Table/TableKlasifikasi/TableKlasifikasi.jsx
@@ -18,8 +18,24 @@ const TableKlasifikasi = (props)=>{
                         <TableRow>
                             <StyledTableCell align="center" width="8%">No.</StyledTableCell>
                             <StyledTableCell align="left">Klasifikasi</StyledTableCell>
+                            <StyledTableCell align="center" width="15%">Peorangan</StyledTableCell>
+                            <StyledTableCell align="center" width="15%">Beregu</StyledTableCell>
                         </TableRow>
                     </TableHead>
+                    <TableBody>
+
+                    </TableBody>
+                    <TableFooter>
+                        <StyledTableCell component="th" scope="row" align="center" colspan={2}>
+                            Total Peserta
+                        </StyledTableCell>
+                        <StyledTableCell component="th" scope="row" align="center">
+                            3
+                        </StyledTableCell>
+                        <StyledTableCell component="th" scope="row" align="center">
+                            3
+                        </StyledTableCell>
+                    </TableFooter>
                 </Table>
             </TableContainer>
         </Box>
